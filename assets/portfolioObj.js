1eda5a69e60be7c1df972d41655987bf6f6087e1
Scott Zinski <zoolunk@gmail.com>
2020-09-16 09:36:25 -0400

    updates portfolio

--- assets/portfolioObj.js
+++ assets/portfolioObj.js
@@ -1,26 +1,33 @@
-const PortfolioItem = function (obj) {
-  this.name = obj.name;
-  this.synopsis = obj.synopsis;
-  this.image = obj.image;
-  this.gitURL = obj.gitURL;
-  this.siteURL = obj.siteURL;
-  this.mobile = obj.mobile;
-  this.tech = obj.tech;
-  this.skip = obj.skip;
+class PortfolioItem {
+	constructor(obj) {
+		this.name = obj.name;
+		this.synopsis = obj.synopsis;
+		this.image = obj.image;
+		this.gitURL = obj.gitURL;
+		this.siteURL = obj.siteURL;
+		this.mobile = obj.mobile;
+		this.tech = obj.tech;
+		this.skip = obj.skip;
+	}
 
-  this.drawCard = function(){
-    let buttons = /*html*/`<a class="btn btn-success btn-block" href="${this.siteURL}" target="_blank" role= "button" aria-pressed="true">Check It Out</a>`
-    if (this.mobile){
-      buttons += /*html*/ `<button class="btn btn-success btn-block modalProject mb-0 d-none d-md-block" type="button" data-url="${this.siteURL}">Mobile</button>`
-    }
-    let wowDuration = ((Math.random()*2) + 1.0).toFixed(2);
-    let techList = ""
-    this.tech.forEach(function(techItem){ // tech boxes
-      if(techItem !== "</br>"){
-        techList += `<p title="Technology">${techItem}</p>`
-      }else{techList += techItem}
-    });
-    let cardCol = /*html*/`
+	drawCard() {
+		let buttons = /*html*/ `<a class="btn btn-success btn-block" href="${this
+			.siteURL}" target="_blank" role= "button" aria-pressed="true">Check It Out</a>`;
+		if (this.mobile) {
+			buttons += /*html*/ `<button class="btn btn-success btn-block modalProject mb-0 d-none d-md-block" type="button" data-url="${this
+				.siteURL}">Mobile</button>`;
+		}
+		let wowDuration = (Math.random() * 2 + 1.0).toFixed(2);
+		let techList = '';
+		this.tech.forEach(function(techItem) {
+			// tech boxes
+			if (techItem !== '</br>') {
+				techList += `<p title="Technology">${techItem}</p>`;
+			} else {
+				techList += techItem;
+			}
+		});
+		let cardCol = /*html*/ `
     <div class="col-md-6 col-xl-4 wow fadeInUp" data-wow-duration="${wowDuration}s" data-wow-offset="100">
       <div class="card mb-4" >
         <img class="card-img-top img-fluid" src="${this.image}" alt="${this.name}">
@@ -32,7 +39,12 @@ const PortfolioItem = function (obj) {
             <div class="row justify-content-center">
               <div class="col-12">
                 ${buttons}
-                <a class="gitTag" href="${this.gitURL}" title="github repository" target="_blank"><i class="fas fa-code"></i></a>
+                <a class="gitTag"
+                  href="${this.gitURL}" 
+                  title="github repository" 
+                  target="_blank">
+                  <i class="fas fa-code"></i>
+                </a>
               </div>
             </div>
           </div>
@@ -42,228 +54,227 @@ const PortfolioItem = function (obj) {
         </div>
       </div>
     </div>
-    `
-    $("#portfolioRow").append(cardCol);
-  };
-};
-
+    `;
+		$('#portfolioRow').append(cardCol);
+	}
+}
 
 var portfolioArr = [
-  {
-    name: "Client Invoice Form",
-    synopsis: "Form for client invoice remittance",
-    image: "assets/images/payment-form-sq.jpg",
-    gitURL: "https://github.com/zkinsk/portfolio",
-    siteURL: "https://safe-river-75201.herokuapp.com/",
-    mobile: true,
-    order: 30,
-    skip: false,
-    tech: ["React", "Axios"]
-  },
-  {
-    name: "Make Room",
-    synopsis: "Richmond Va Makers - Event Scheduler & Portoflio Platform",
-    image: "assets/images/make-room.jpg",
-    gitURL: "https://github.com/zkinsk/Make-Room",
-    siteURL: "https://make-room-3.herokuapp.com/",
-    mobile: true,
-    order: 10,
-    skip: false,
-    tech: ["Mongo.db", "AWS", "NodeJs","Express", "</br>", "Axios", "React"]
-  },
-  {
-    name: "Google Books Search",
-    synopsis: "React running with a Node.js server hitting google books API and saving data to Mongo DB - What more it there to say? ",
-    image: "assets/images/winesburg-ohio.jpg",
-    gitURL: "https://github.com/zkinsk/books-search",
-    siteURL: "https://enigmatic-cliffs-12223.herokuapp.com/",
-    mobile: true,
-    order: 900,
-    skip: true,
-    tech: ["Mongo", "Mongoose", "NodeJs","Express", "</br>", "Axios", "React"]
-  },
-  {
-    name: "Scrape This",
-    synopsis: "Mongo DB, Axios and Cheerio working together to scrape you some data",
-    image: "assets/images/single-track.jpg",
-    gitURL: "https://github.com/zkinsk/scrape-this",
-    siteURL: "https://scrape-this-sz.herokuapp.com",
-    mobile: true,
-    order: 40,
-    skip: false,
-    tech: ["Mongo", "Mongoose", "NodeJs","Express", "</br>", "Axios", "Cheerio", "Handlebars"]
-  },
-  {
-    name: "Bob Ross Word Guess",
-    synopsis: "Hangman styled word guess game with an homage to Bob Ross and his Happy Little Trees... Also my first ever code and not mobile ready",
-    image: "assets/images/bob-ross_header.jpg",
-    gitURL: "https://github.com/zkinsk/Word-Guess-Game",
-    siteURL: "https://zkinsk.github.io/Word-Guess-Game/",
-    mobile: false,
-    order: 200,
-    skip: true,
-    tech: ["HTML", "CSS", "Javascript"]
-  },
-  {
-    name: "Crystals Collector",
-    synopsis: "A funny little game that leaves you guessing as to how to match the value of four unknown crystals to a target number.",
-    image: "assets/images/Garnet_Andradite20.jpg",
-    gitURL: "https://github.com/zkinsk/unit-4-gemgame",
-    siteURL: "https://zkinsk.github.io/unit-4-gemgame/",
-    mobile: false,
-    order: 70,
-    skip: true,
-    tech: ["HTML", "CSS", "jQuery"]
-  },
-  {
-    name: "Bike to Brew",
-    synopsis: "Find good brews near to your favorite trail, or explore both someplace else!",
-    image: "assets/images/biketobrew_square.png",
-    gitURL: "https://github.com/zkinsk/BikeToBrew",
-    siteURL: "https://zkinsk.github.io/BikeToBrew/",
-    mobile: true,
-    order: 20,
-    skip: false,
-    tech: ["Materialize", "AJAX & APIs", "jQuery"]
-  },
-  {
-    name: "Star Wars RPG",
-    synopsis: "Pick your favorite Jedi, and do battle with 3 others. Turn up the volume!",
-    image: "assets/images/the-empire-strikes-back-star-wars.jpg",
-    gitURL: "https://github.com/zkinsk/unit-4-RPGgame",
-    siteURL: "https://zkinsk.github.io/unit-4-RPGgame/",
-    mobile: false,
-    order: 60,
-    skip: true,
-    tech: ["HTML", "Bootstrap", "jQuery"]
-  },
-  {
-    name: "Big Lebowski Trivia",
-    synopsis: "The Dude abides and so should you. Don't roll any gutter balls!",
-    image: "assets/images/big_Lebowski.jpg",
-    gitURL: "https://github.com/zkinsk/TriviaGame",
-    siteURL: "https://zkinsk.github.io/TriviaGame/",
-    mobile: false,
-    order: 50,
-    skip: true,
-    tech: ["HTML", "Bootstrap", "jQuery"]
-  },
-  {
-    name: "Gif Viewer",
-    synopsis: "A nice teal and orange gif viewer using the Giphy API.",
-    image: "assets/images/mtb-Bike.gif",
-    gitURL: "https://github.com/zkinsk/Giphy-HW",
-    siteURL: "https://zkinsk.github.io/Giphy-HW/",
-    mobile: true,
-    order: 25,
-    skip: true,
-    tech: ["AJAX & APIs", "Bootstrap", "jQuery"]
-  },
-  {
-    name: "MultiPlayer Rock Paper Sissors",
-    synopsis: "Play rock paper sissors with your best buds and chat smack while your crack some sissors",
-    image: "assets/images/RockPaperScissors.jpg",
-    gitURL: "assets/images/RockPaperScissors.jpg",
-    siteURL: "https://zkinsk.github.io/RPS-Multiplayer/",
-    mobile: true,
-    order: 60,
-    skip: false,
-    tech: ["Firebase", "jQuery", "Bootstrap"]
-  },
-  {
-    name: "liriBot",
-    synopsis: "Console Line bot to find info on movies, conerts and songs.",
-    image: "assets/images/siri-2.gif",
-    gitURL: "https://github.com/zkinsk/liri-node-app",
-    siteURL: "https://github.com/zkinsk/liri-node-app",
-    mobile: false,
-    order: 80,
-    skip: true,
-    tech: ["nodeJS", "AJAX"]
-  },
-  {
-    name: "Node Word Guess",
-    synopsis: "Command line constructor Word Guess game built with node.js",
-    image: "assets/images/constructor_word_guess.png",
-    gitURL: "https://github.com/zkinsk/Constructor-WordGuess",
-    siteURL: "https://github.com/zkinsk/Constructor-WordGuess",
-    mobile: false,
-    order: 90,
-    skip: true,
-    tech: ["Constructors", "nodeJS"]
-  },
-  {
-    name: "bAmazon",
-    synopsis: "Definitely not Amazon online store using MySql and node.js",
-    image: "assets/images/bamazonBox.jpg",
-    gitURL: "https://github.com/zkinsk/bamazon",
-    siteURL: "https://github.com/zkinsk/bamazon",
-    mobile: false,
-    order: 100,
-    skip: true,
-    tech: ["nodeJS", "mySQL"],
-  },
-  {
-    name: "Friend Finder",
-    synopsis: "You'll never guess who you could be friends with! You're just 10 survey questions away from a new bestie.",
-    image: "assets/images/friend-finder.jpg",
-    gitURL: "https://github.com/zkinsk/friendFinder",
-    siteURL: "https://friend-finder-sz-2.herokuapp.com/",
-    mobile: true,
-    order: 110,
-    skip: true,
-    tech: ["nodeJS", "Heroku", "express", "handlebars"]
-  },
-  {
-    name: "Eat Dat Burger",
-    synopsis: "Create a Burger, Chomp a Burger and watch the database update!",
-    image: "assets/images/burger.gif",
-    gitURL: "https://github.com/zkinsk/burger",
-    siteURL: "https://burgerize-scz.herokuapp.com/",
-    mobile: true,
-    order: 120,
-    skip: true,
-    tech: ["ORM", "mySQL", "nodeJS", "express", "handlebars"]
-  },
-  {
-    name: "Dogs Day Out",
-    synopsis: "Chat with some folks, check out their dogs stats, and set up a play date!",
-    image: "assets/images/two-pugs.jpg",
-    gitURL: "https://github.com/zkinsk/project-2",
-    siteURL: "https://dogs-day-out-2.herokuapp.com/",
-    mobile: true,
-    order: 10,
-    skip: true,
-    tech: ["nodeJS", "express", "sequelize", "AWS", "Heroku", "</br>", "Bulma", "Handlebars"]
-  },
-  {
-    name: "Steven React",
-    synopsis: "Simple Steven Universe Memory Game - Built with React",
-    image: "assets/images/garnet-fusion.gif",
-    gitURL: "https://github.com/zkinsk/react-memory-game",
-    siteURL: "https://young-sierra-70479.herokuapp.com/",
-    mobile: true,
-    order: 50,
-    skip: false,
-    tech: ["React", "Bootstrap"]
-  },
-];//end of portfolio objectArray
+	{
+		name: 'Client Invoice Form',
+		synopsis: 'Form for client invoice remittance',
+		image: 'assets/images/payment-form-sq.jpg',
+		gitURL: 'https://github.com/zkinsk/portfolio',
+		siteURL: 'https://safe-river-75201.herokuapp.com/',
+		mobile: true,
+		order: 30,
+		skip: false,
+		tech: [ 'React', 'Axios' ]
+	},
+	{
+		name: 'Make Room',
+		synopsis: 'Richmond Va Makers - Event Scheduler & Portoflio Platform',
+		image: 'assets/images/make-room.jpg',
+		gitURL: 'https://github.com/zkinsk/Make-Room',
+		siteURL: 'https://make-room-3.herokuapp.com/',
+		mobile: true,
+		order: 10,
+		skip: false,
+		tech: [ 'Mongo.db', 'AWS', 'NodeJs', 'Express', '</br>', 'Axios', 'React' ]
+	},
+	{
+		name: 'Google Books Search',
+		synopsis:
+			'React running with a Node.js server hitting google books API and saving data to Mongo DB - What more it there to say? ',
+		image: 'assets/images/winesburg-ohio.jpg',
+		gitURL: 'https://github.com/zkinsk/books-search',
+		siteURL: 'https://enigmatic-cliffs-12223.herokuapp.com/',
+		mobile: true,
+		order: 900,
+		skip: true,
+		tech: [ 'Mongo', 'Mongoose', 'NodeJs', 'Express', '</br>', 'Axios', 'React' ]
+	},
+	{
+		name: 'Scrape This',
+		synopsis: 'Mongo DB, Axios and Cheerio working together to scrape you some data',
+		image: 'assets/images/single-track.jpg',
+		gitURL: 'https://github.com/zkinsk/scrape-this',
+		siteURL: 'https://scrape-this-sz.herokuapp.com',
+		mobile: true,
+		order: 40,
+		skip: false,
+		tech: [ 'Mongo', 'Mongoose', 'NodeJs', 'Express', '</br>', 'Axios', 'Cheerio', 'Handlebars' ]
+	},
+	{
+		name: 'Bob Ross Word Guess',
+		synopsis:
+			'Hangman styled word guess game with an homage to Bob Ross and his Happy Little Trees... Also my first ever code and not mobile ready',
+		image: 'assets/images/bob-ross_header.jpg',
+		gitURL: 'https://github.com/zkinsk/Word-Guess-Game',
+		siteURL: 'https://zkinsk.github.io/Word-Guess-Game/',
+		mobile: false,
+		order: 200,
+		skip: true,
+		tech: [ 'HTML', 'CSS', 'Javascript' ]
+	},
+	{
+		name: 'Crystals Collector',
+		synopsis:
+			'A funny little game that leaves you guessing as to how to match the value of four unknown crystals to a target number.',
+		image: 'assets/images/Garnet_Andradite20.jpg',
+		gitURL: 'https://github.com/zkinsk/unit-4-gemgame',
+		siteURL: 'https://zkinsk.github.io/unit-4-gemgame/',
+		mobile: false,
+		order: 70,
+		skip: true,
+		tech: [ 'HTML', 'CSS', 'jQuery' ]
+	},
+	{
+		name: 'Bike to Brew',
+		synopsis: 'Find good brews near to your favorite trail, or explore both someplace else!',
+		image: 'assets/images/biketobrew_square.png',
+		gitURL: 'https://github.com/zkinsk/BikeToBrew',
+		siteURL: 'https://zkinsk.github.io/BikeToBrew/',
+		mobile: true,
+		order: 20,
+		skip: false,
+		tech: [ 'Materialize', 'AJAX & APIs', 'jQuery' ]
+	},
+	{
+		name: 'Star Wars RPG',
+		synopsis: 'Pick your favorite Jedi, and do battle with 3 others. Turn up the volume!',
+		image: 'assets/images/the-empire-strikes-back-star-wars.jpg',
+		gitURL: 'https://github.com/zkinsk/unit-4-RPGgame',
+		siteURL: 'https://zkinsk.github.io/unit-4-RPGgame/',
+		mobile: false,
+		order: 60,
+		skip: true,
+		tech: [ 'HTML', 'Bootstrap', 'jQuery' ]
+	},
+	{
+		name: 'Big Lebowski Trivia',
+		synopsis: "The Dude abides and so should you. Don't roll any gutter balls!",
+		image: 'assets/images/big_Lebowski.jpg',
+		gitURL: 'https://github.com/zkinsk/TriviaGame',
+		siteURL: 'https://zkinsk.github.io/TriviaGame/',
+		mobile: false,
+		order: 50,
+		skip: true,
+		tech: [ 'HTML', 'Bootstrap', 'jQuery' ]
+	},
+	{
+		name: 'Gif Viewer',
+		synopsis: 'A nice teal and orange gif viewer using the Giphy API.',
+		image: 'assets/images/mtb-Bike.gif',
+		gitURL: 'https://github.com/zkinsk/Giphy-HW',
+		siteURL: 'https://zkinsk.github.io/Giphy-HW/',
+		mobile: true,
+		order: 25,
+		skip: true,
+		tech: [ 'AJAX & APIs', 'Bootstrap', 'jQuery' ]
+	},
+	{
+		name: 'MultiPlayer Rock Paper Sissors',
+		synopsis: 'Play rock paper sissors with your best buds and chat smack while your crack some sissors',
+		image: 'assets/images/RockPaperScissors.jpg',
+		gitURL: 'assets/images/RockPaperScissors.jpg',
+		siteURL: 'https://zkinsk.github.io/RPS-Multiplayer/',
+		mobile: true,
+		order: 60,
+		skip: false,
+		tech: [ 'Firebase', 'jQuery', 'Bootstrap' ]
+	},
+	{
+		name: 'liriBot',
+		synopsis: 'Console Line bot to find info on movies, conerts and songs.',
+		image: 'assets/images/siri-2.gif',
+		gitURL: 'https://github.com/zkinsk/liri-node-app',
+		siteURL: 'https://github.com/zkinsk/liri-node-app',
+		mobile: false,
+		order: 80,
+		skip: true,
+		tech: [ 'nodeJS', 'AJAX' ]
+	},
+	{
+		name: 'Node Word Guess',
+		synopsis: 'Command line constructor Word Guess game built with node.js',
+		image: 'assets/images/constructor_word_guess.png',
+		gitURL: 'https://github.com/zkinsk/Constructor-WordGuess',
+		siteURL: 'https://github.com/zkinsk/Constructor-WordGuess',
+		mobile: false,
+		order: 90,
+		skip: true,
+		tech: [ 'Constructors', 'nodeJS' ]
+	},
+	{
+		name: 'bAmazon',
+		synopsis: 'Definitely not Amazon online store using MySql and node.js',
+		image: 'assets/images/bamazonBox.jpg',
+		gitURL: 'https://github.com/zkinsk/bamazon',
+		siteURL: 'https://github.com/zkinsk/bamazon',
+		mobile: false,
+		order: 100,
+		skip: true,
+		tech: [ 'nodeJS', 'mySQL' ]
+	},
+	{
+		name: 'Friend Finder',
+		synopsis:
+			"You'll never guess who you could be friends with! You're just 10 survey questions away from a new bestie.",
+		image: 'assets/images/friend-finder.jpg',
+		gitURL: 'https://github.com/zkinsk/friendFinder',
+		siteURL: 'https://friend-finder-sz-2.herokuapp.com/',
+		mobile: true,
+		order: 110,
+		skip: true,
+		tech: [ 'nodeJS', 'Heroku', 'express', 'handlebars' ]
+	},
+	{
+		name: 'Eat Dat Burger',
+		synopsis: 'Create a Burger, Chomp a Burger and watch the database update!',
+		image: 'assets/images/burger.gif',
+		gitURL: 'https://github.com/zkinsk/burger',
+		siteURL: 'https://burgerize-scz.herokuapp.com/',
+		mobile: true,
+		order: 120,
+		skip: true,
+		tech: [ 'ORM', 'mySQL', 'nodeJS', 'express', 'handlebars' ]
+	},
+	{
+		name: 'Dogs Day Out',
+		synopsis: 'Chat with some folks, check out their dogs stats, and set up a play date!',
+		image: 'assets/images/two-pugs.jpg',
+		gitURL: 'https://github.com/zkinsk/project-2',
+		siteURL: 'https://dogs-day-out-2.herokuapp.com/',
+		mobile: true,
+		order: 10,
+		skip: true,
+		tech: [ 'nodeJS', 'express', 'sequelize', 'AWS', 'Heroku', '</br>', 'Bulma', 'Handlebars' ]
+	},
+	{
+		name: 'Steven React',
+		synopsis: 'Simple Steven Universe Memory Game - Built with React',
+		image: 'assets/images/garnet-fusion.gif',
+		gitURL: 'https://github.com/zkinsk/react-memory-game',
+		siteURL: 'https://young-sierra-70479.herokuapp.com/',
+		mobile: true,
+		order: 50,
+		skip: false,
+		tech: [ 'React', 'Bootstrap' ]
+	}
+]; //end of portfolio objectArray
 
+function drawPortolio() {
+	portfolioArr.sort((a, b) => a.order - b.order);
+	portfolioArr.forEach(portItem => {
+		let portCard = new PortfolioItem(portItem);
+		if (!portCard.skip) {
+			portCard.drawCard();
+		}
+	});
+} //end of drawPorfolio fn
 
-function drawPortolio(){
-  portfolioArr.sort((a, b) => a.order - b.order)
-  portfolioArr.forEach(portItem => {
-    let portCard = new PortfolioItem(portItem)
-    if (!portCard.skip){
-      portCard.drawCard();
-    }
-  });
-}//end of drawPorfolio fn
-
-$(document).ready(function(){
-  drawPortolio();
-// end of doc ready
-})
-
-
-
+$(document).ready(function() {
+	drawPortolio();
+	// end of doc ready
+});
